de2ec74fe2c90af4c61883266b96fef8540ec56a
natural-public <35077585+natural-public@users.noreply.github.com>
2018-02-09 20:18:43 +0200

    Update jQ-natural_public-advertising.js

--- jQ/jQ-natural_public-advertising.js
+++ jQ/jQ-natural_public-advertising.js
@@ -11,4 +11,41 @@ $(document).ready(function(){
 	  $('body,html').animate({scrollTop:0},700);
 	  });
 	});
+	
+	$("#div-table1").click(function(){
+        $("#open_menu1").slideToggle("slow");
+        $(this).toggleClass("active"); return false;
+    });
+	
+	$("#div-table2").click(function(){
+        $("#open_menu2").slideToggle("slow");
+        $(this).toggleClass("active"); return false;
+    });
+	
+	$("#div-table3").click(function(){
+        $("#open_menu3").slideToggle("slow");
+        $(this).toggleClass("active"); return false;
+    });
+	
+	/*arrow*/
+	
+	$("#div-table1").click(function(){
+        $(".img_menu1").slideToggle("hide");
+        $(".img_menu2").slideToggle("slow");
+        $(this).toggleClass("active"); return false;
+    });
+	
+		$("#div-table2").click(function(){
+        $(".img_menu3").slideToggle("hide");
+        $(".img_menu4").slideToggle("slow");
+        $(this).toggleClass("active"); return false;
+    });
+	
+		$("#div-table3").click(function(){
+        $(".img_menu5").slideToggle("hide");
+        $(".img_menu6").slideToggle("slow");
+        $(this).toggleClass("active"); return false;
+    });
+	
+
 });
